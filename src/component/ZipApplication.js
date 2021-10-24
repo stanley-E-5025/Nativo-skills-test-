e036c31cb518b1b85ebd68030596428e75b5fa3b
stanley <stanleygordon45@gmail.com>
2021-10-24 17:10:27 -0600

    frontend done

--- src/component/ZipApplication.js
+++ src/component/ZipApplication.js
@@ -2,13 +2,24 @@ import React, { useState } from "react";
 import "../css/App.css";
 import ZipList from "./ZipList";
 import ZipError from "./ZipError";
+import data from "../data/ZipCodes";
 
 export default function ZipApplication() {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
+
+  const [resultState, setResultState] = useState({
+    status: "",
+  });
+
+  const match =
+    data.filter(function (e) {
+      if (e.code === value) return true;
+      else return false;
+    }).length > 0;
 
   return (
     <div className="  container-fluid   Home-container ">
-      <div className="  Home-top-bar-container  ">
+      <div className="  Home-top-bar-container">
         <div className="  Home-top-bar-title">
           <span className="  Home-top-bar-title-color">Zip.</span>
           App
@@ -26,35 +37,94 @@ export default function ZipApplication() {
       </div>
 
       <div className="  container   Home-items-container  ">
-        <div className="  row  ">
+        <div className="   row  ">
           <div className="  col-md-12   Home-search-container">
-            <input
-              placeholder="Search "
-              className="  Home-text-input"
-              type="number"
-              min="0"
-              step="1"
-              value={value && Math.max(0, value)}
-              onChange={(e) => setValue(e.target.value)}
-            />
+            <div className="   Home-input-container">
+              <input
+                placeholder="Search "
+                className="  Home-text-input"
+                type="text"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </div>
+            <div className="Home-auto-complete-container">
+              {data
+                .filter((data) => data.code === value)
+                .map((data) => {
+                  return (
+                    <button
+                      className="Home-auto-complete-option"
+                      key={data.code}
+                      tabIndex="0"
+                      onClick={() =>
+                        setResultState({
+                          code: data.code,
+                          status: "match",
+                          departamento: data.departamento,
+                          municipio: data.municipio,
+                          barrio: data.barrio,
+                          error: "",
+                        })
+                      }
+                    >
+                      {data.departamento} , {data.barrio},{data.code}
+                    </button>
+                  );
+                })}
+            </div>
           </div>
         </div>
 
         <div className="  row  ">
           <div className="  col-md-5   Home-result-container">
             <div className="  Home-bottom-cards">
-              <div className="   border Home-card-title">All Zip Codes</div>
+              <div className="      Home-card-title">results</div>
+              <div className="  Home-card-scroll-container">
+                {resultState.status === "" && <ZipError error={match} />}
+                {resultState.status === "match" && match === false && (
+                  <ZipError error={match} />
+                )}
 
-              <div className="     Home-card-scroll-container">
-                <ZipList />
+                {resultState.status === "match" &&
+                  match === false &&
+                  setResultState({ status: "" })}
+                {resultState.status === "match" && match === true && (
+                  <div className="   Home-card-match-container">
+                    <div className="   Home-card-match-container-Item">
+                      <div className="Home-list-left-info">
+                        <div className="Home-list-left-title">
+                          {resultState.departamento} , {resultState.municipio}
+                        </div>
+
+                        <div>{resultState.barrio}</div>
+                      </div>
+
+                      <div className="Home-list-right-info-match  ">
+                        <div className="Home-list-left-title">
+                          {" "}
+                          ZIP {resultState.code}{" "}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="Home-match-image-container  ">
+                      <img
+                        alt="User  "
+                        className="Home-card-image-err-match"
+                        src="https://siasky.net/_ATBFW5aONufqUScyqkSnWN1IbofC__lhLBpMZ59ltqqeg"
+                      />
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>
           <div className="  col-md-7   Home-result-container">
             <div className="  Home-bottom-cards">
-              <div className="   border  Home-card-title">results</div>
-              <div className="  Home-card-scroll-container">
-                <ZipError />
+              <div className="     Home-card-title">All Zip Codes</div>
+
+              <div className="     Home-card-scroll-container">
+                <ZipList />
               </div>
             </div>
           </div>
--- src/component/ZipError.js
+++ src/component/ZipError.js
@@ -1,17 +1,34 @@
 import React from "react";
 
-export default function ZipError() {
+export default function ZipError({ error }) {
   return (
     <div className="   Home-Zip-Error">
-      <img
-        alt="User  "
-        className="Home-card-image-err"
-        src="https://i.pinimg.com/originals/e5/2b/fa/e52bfabd131ce16b7ccb1c30dda657e6.png"
-      />
-
+      {error === false && (
+        <img
+          alt="User  "
+          className="Home-card-image-err"
+          src="https://i.pinimg.com/originals/e5/2b/fa/e52bfabd131ce16b7ccb1c30dda657e6.png"
+        />
+      )}
       <div className="Home-card-txt-err">
-        Oops, try to write something first
-        <i className="fas fa-exclamation-triangle Error-Icon"></i>
+        {error === true && (
+          <div className="Home-card-match-result">
+            {" "}
+            nice! now select the autocomplete result
+            <i className="far fa-check-circle H-I"></i>
+          </div>
+        )}
+
+        {error === false && (
+          <>
+            <div>
+              {
+                "  Oops, try to write something first or try to write a valid code "
+              }{" "}
+              <i className="fas fa-exclamation-triangle Error-Icon"></i>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
